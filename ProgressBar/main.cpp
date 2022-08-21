03779a3defa0c437e4011154f53dd0d0f11b5ea9
carnacf <florian.carnac@gmail.com>
2022-08-21 13:55:28 +0200

    Add openmp test

--- ProgressBar/main.cpp
+++ ProgressBar/main.cpp
@@ -3,14 +3,18 @@
 
 #include <chrono>
 #include <thread>
+#include <iostream>
 
 #include <QApplication>
 #include <QMainwindow>
 #include <QtWidgets>
 
+#include <omp.h>
+
 void dummyFunc(int nbI = 100, int nbJ = 100, const IProgressReporter& reporter = EmptyProgressReporter())
 {
     auto mainHandler = reporter.startReport("Dummy", nbI);
+
     for (int i = 0; i < nbI; i++)
     {
         if (reporter.isCanceled())
@@ -35,8 +39,41 @@ void startComputation()
 {
     ProgressDialog dialog;
     auto& handler = dialog.getReporter();
-    //dummyFunc(10, 10, handler);
     dialog.exec(dummyFunc, 10, 10, std::ref(handler));
+}
+
+void dummyFuncWithOmp(int nbI = 100, int nbJ = 100, const IProgressReporter& reporter = EmptyProgressReporter())
+{
+    auto mainHandler = reporter.startReport("Dummy", nbI);
+
+#pragma omp parallel for
+    for (int i = 0; i < nbI; i++)
+    {
+        if (reporter.isCanceled())
+            break;
+
+        std::cout << "Thread : " << i << std::endl;
+        std::string label = std::string("Dummy internal i:") + std::to_string(i);
+        auto internalHAndler = reporter.startReport(label, nbJ);
+        internalHAndler->setAutoClose(false);
+        for (int j = 0; j < nbJ; j++)
+        {
+            if (reporter.isCanceled())
+                break;
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            internalHAndler->increment();
+        }
+
+        mainHandler->increment();
+    }
+}
+
+void startComputationWithomp()
+{
+    ProgressDialog dialog;
+    auto& handler = dialog.getReporter();
+    dialog.exec(dummyFuncWithOmp, 10, 10, std::ref(handler));
 }
 
 int main(int argc, char *argv[])
@@ -44,10 +81,22 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QMainWindow mainWindow;
     
-    QPushButton* button = new QPushButton("Start");
-    QObject::connect(button, &QPushButton::clicked, &startComputation);
+    QWidget* mainWidget = new QWidget(&mainWindow);
+    mainWindow.setCentralWidget(mainWidget);
 
-    mainWindow.setCentralWidget(button);
+    QVBoxLayout* layout = new QVBoxLayout();
+    mainWidget->setLayout(layout);
+
+    QPushButton* button1 = new QPushButton("Test");
+    QObject::connect(button1, &QPushButton::clicked, &startComputation);
+    layout->addWidget(button1);
+
+    QPushButton* button2 = new QPushButton("Test with omp multithreading");
+    QObject::connect(button2, &QPushButton::clicked, &startComputationWithomp);
+    layout->addWidget(button2);
+
+
+
     mainWindow.show();
 
     return a.exec();
